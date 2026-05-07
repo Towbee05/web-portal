3c329613bd8cac303fde3ee8f3c4752d86607b6f
Towbee05 <olatiseoluwatobiloba@gmail.com>
2026-05-07 13:59:58 +0100

    (feat) Completed signup flow

--- hng-web-portal/app/dashboard/page.tsx
+++ hng-web-portal/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
-// "use client";
-// import { useState } from "react";
-// import authApiAdapter from "@/lib/authApiAdapter.ts";
+"use client";
+import { useState } from "react";
+import authApiAdapter from "@/lib/authApiAdapter.ts";
 
-// export default function Home() {
-//   const [error, setError] = useState(false);
-// }
+export default function Home() {
+  const [error, setError] = useState(false);
+  return (
+    <>
+      <h1> Who am I?? </h1>
+    </>
+  );
+}
